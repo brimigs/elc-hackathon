368c51fc0daba8cd94c75c99685b2383ccff2e74
brimigs <85972460+brimigs@users.noreply.github.com>
2021-10-23 21:46:18 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,15 +3,6 @@ import Footer from './Components/Footer';
 import Header from './Components/Header';
 import * as React from 'react';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-// {if (window.location.href === "App") <AppHeader />}
-
 function App() {
   return (
     <div id="wrapper">
